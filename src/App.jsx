7a70e27a03b97437aad69d0b64b79fea12414fdf
Lati Tibabu <latitibabu2018@gmail.com>
2024-11-19 11:20:20 +0300

    feat: to do will be returned in a table format

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
+import axios from "axios";
+import { Input } from "postcss";
+import { useEffect, useState } from "react";
 import { AiOutlineSend } from "react-icons/ai";
 
 function App() {
+  const apiURL = "http://localhost:3002" || import.meta.env.API_URL;
+
+  const [prompt, setPrompt] = useState("");
+  const [aiResponse, setAiResponse] = useState([]);
+  const [aiProcessing, setAiProcessing] = useState(false);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setAiProcessing(!aiProcessing);
+    // console.log(prompt)
+    // alert(apiURL);
+    try {
+      const response = await axios.post(`${apiURL}/generate-todo`, {
+        input: prompt,
+      });
+      // console.log(response);
+
+      if (response.status === 200) {
+        const todo = response.data.response;
+        setAiResponse(todo);
+        console.log(todo);
+      } else {
+        console.log("error occurred!");
+      }
+
+      // console.log(response.data.response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    // console.log(aiResponse);
+    setAiProcessing(!aiProcessing);
+  }, [aiResponse]);
+
   return (
     <div className="font-mono bg-gray-50 w-screen h-screen m-0 text-black">
       <div className="w-fit mx-auto p-10">
         <div>
-          <h1 className="text-black font-bold text-3xl">My AI TO-DO help</h1>
+          <h1 className="text-black font-bold text-3xl">✨ My AI TO-DO help</h1>
         </div>
 
         <div className="mt-5">
@@ -16,13 +55,18 @@ function App() {
           >
             {/* text area for prompt */}
             <textarea
-              className="textarea textarea-bordered bg-transparent text-gray-700"
+              className="textarea textarea-bordered bg-transparent text-gray-700 w-full h-fit"
               placeholder="Enter your prompt"
+              onChange={(e) => setPrompt(e.target.value)}
             ></textarea>
             {/* send button */}
-            <button className="btn btn-active btn-default">
-              <AiOutlineSend />
-            </button>
+            {aiProcessing ? (
+              <span className="loading loading-infinity"></span>
+            ) : (
+              <button className="btn btn-active btn-default">
+                <AiOutlineSend />
+              </button>
+            )}
           </form>
         </div>
 
@@ -41,27 +85,26 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                {/* row 1 */}
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                </tr>
-                {/* row 2 */}
-                <tr>
-                  <th>2</th>
-                  <td>Hart Hagerty</td>
-                  <td>Desktop Support Technician</td>
-                  <td>Purple</td>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                  <th>3</th>
-                  <td>Brice Swyre</td>
-                  <td>Tax Accountant</td>
-                  <td>Red</td>
-                </tr>
+                {aiResponse.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      {/* <th>{index + 1}</th> */}
+                      <td>
+                        <label className="label cursor-pointer flex flex-row gap-4">
+                          <span className="label-text">Done</span>
+                          <input
+                            type="checkbox"
+                            defaultChecked={false}
+                            className="checkbox"
+                          />
+                        </label>
+                      </td>
+                      <td>{item.title}</td>
+                      <td>{item.description}</td>
+                      <td>{item.due_date}</td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
